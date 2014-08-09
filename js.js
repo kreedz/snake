2a05e1bdc96a1b5fed7b7c231d9e5693de290d6d
kreedz <edgebug@gmail.com>
2014-08-09 18:59:54 +0600

    added three classes: Game, Field, Food Snake inherited Food

--- js.js
+++ js.js
@@ -1,13 +1,119 @@
 ﻿(function() {
-    var canvas = document.getElementById('canvas'),
-        ctx = canvas.getContext('2d');
-    canvas.width  = 640;
-    canvas.height = 480;
-    canvas.addEventListener('click', function() {
-        start();
-    }, false);
+    var canvas = document.getElementById('canvas');
+
+    function extend(Child, Parent) {
+        var F = function() {}
+        F.prototype = Parent.prototype;
+        Child.prototype = new F();
+        Child.prototype.constructor = Child;
+        Child.superclass = Parent.prototype;
+    }    
     
-    var snake;
+    function Game(canvas) {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext('2d');
+        this.canvas.addEventListener('click', function() {
+            this.start();
+        }.bind(this), false);
+    }
+    
+    Game.cfg = {
+        'width': 20, 'speed': 0.1, 'length': 5,
+        'field': {
+            'width': 640, 'height': 480
+        },
+        'food': {
+            'length': 1
+        }
+    }
+    
+    Game.prototype.start = function() {
+        var cfg = Game.cfg;
+        if (typeof this.field != 'undefined') {
+            delete this.field;
+        }
+        this.field = new Field(this.canvas, cfg.field);
+        if (typeof this.food != 'undefined') {
+            delete this.food;
+        }
+        this.food = new Food(this.canvas, this.ctx, cfg.food.length, cfg.width);
+        this.food.game();
+        if (typeof this.snake != 'undefined') {
+            this.snake.stop();
+            delete this.snake;
+        }
+        this.snake = new Snake(this.canvas, this.ctx, cfg.speed, cfg.length, cfg.width);
+        this.snake.game();
+    }
+    
+    Game.prototype.cleanCanvas = function() {
+        this.ctx.fillStyle = 'white';
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.width);
+        this.ctx.fillStyle = 'red';
+    }
+
+    function Field(canvas, sizes) {
+        canvas.width  = sizes.width;
+        canvas.height = sizes.height;
+    }
+    
+    function Food(canvas, ctx, length, width) {
+        this.canvas = canvas;
+        this.ctx = ctx;
+        this.width = width > 0 ? width : 1;
+        this.length = length > 0 ? length : 1;
+        this.body = [];
+    } 
+    
+    Food.prototype.game = function() {
+        this.build('random');
+        this.draw();    
+    }
+    
+    Food.prototype.build = function(place) {
+        var getRandom = function(max) {
+            var rand = Math.floor(Math.random() * max + this.width),
+                sub = rand % this.width;
+            return rand - sub;
+        }.bind(this);
+        var x = this.canvas.width / 2,
+            y = this.canvas.height / 2;
+        if (place == 'random') {
+            x = getRandom(this.canvas.width);
+            y = getRandom(this.canvas.height);
+        }
+        for (var i = 0; i < this.length; ++i) {
+            this.body.push([x, y + i * this.width]);
+        }        
+    }
+    
+    Food.prototype.draw = function() {
+        this.ctx.fillStyle = 'red'; 
+        for (var i = 0; i < this.length; ++i) {
+            this.ctx.fillRect(this.body[i][0], this.body[i][1], this.width, this.width);
+        }    
+    }    
+    
+    Food.prototype.clean = function() {
+        this.ctx.fillStyle = 'white';
+        for(var i = 0; i < this.length; ++i) {
+            this.ctx.fillRect(this.body[i][0], this.body[i][1], this.width, this.width);
+        }
+        this.ctx.fillStyle = 'red';
+    }
+    
+    function Snake(canvas, ctx, speed, length, width) {
+        Snake.superclass.constructor.call(this, canvas, ctx, width);
+        this.speed = speed > 0 ? speed : 1;
+        this.length = length > 0 ? length : 1;
+        this.width = width > 0 ? width : 1;
+        this.body = [];
+        this.ctx = ctx;
+        this.ctx.fillStyle = 'red';
+        this.direction = this.path['up'];
+    }
+    
+    extend(Snake, Food);
     
     Snake.prototype.path = {
         'left'  : 37,
@@ -15,57 +121,6 @@
         'right' : 39,
         'down'  : 40
     };
-    
-    function Snake(canvas, ctx, speed, length, width) {
-        var _speed,
-            _length, 
-            _width;
-        Object.defineProperties(this, {
-            'speed': {
-                get: function() {return _speed},
-                set: function(value) {_speed = value > 0 ? value : 1;},
-            },
-            'length': {
-                get: function() {return _length},           
-                set: function(value) {_length = value > 0 ? value : 1;},
-            },
-            'width': {
-                get: function() {return _width},
-                set: function(value) {_width = value > 0 ? value : 1;},
-            },
-        });
-        this.speed = speed;
-        this.length = length;
-        this.width = width;
-        this.ctx = ctx;
-        this.ctx.fillStyle = 'red';
-        this.body = [];
-        this.canvas = canvas;
-        this.direction = this.path['up'];
-    }
-    
-    Snake.prototype.build = function() {
-        this.clearCanvas();
-        var canvasWidthDivTwo = canvas.width / 2,
-            canvasHeightDivTwo = canvas.height / 2;
-        for (var i = 0; i < this.length; ++i) {
-            this.body.push([canvasWidthDivTwo, canvasHeightDivTwo + i * this.width]);
-        }
-    }
-    
-    Snake.prototype.draw = function() {
-        for (var i = 0; i < this.length; ++i) {
-            var x = this.body[i][0],
-                y = this.body[i][1];
-            this.ctx.fillRect(x, y, this.width, this.width);
-        }    
-    }
-    
-    Snake.prototype.clearCanvas = function() {
-        this.ctx.fillStyle = 'white';
-        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-        this.ctx.fillStyle = 'red';
-    }
     
     Snake.prototype.cleanTail = function() {
         var body = this.body;
@@ -156,6 +211,7 @@
         this.ctx.textAlign = 'center';
         var text = 'YOU DIED!';
         this.ctx.fillText(text, canvas.width / 2, canvas.height / 2);
+        this.ctx.fillStyle = 'red';
     }
     
     Snake.prototype.motion = function() {
@@ -174,7 +230,7 @@
             }
             return false;
         }
-        this.build();
+        this.build('center');
         document.onkeydown = function(e) {
             if (inPath.call(this, e.keyCode)) {
                 this.direction = e.keyCode;
@@ -182,15 +238,7 @@
         }.bind(this);
         this.motion();
     }
-    
-    function start() {
-        if (typeof snake != 'undefined') {
-            snake.stop();
-            delete snake;
-        }
-        snake = new Snake(canvas, ctx, 0.1, 5, 20);
-        snake.game();
-    }
-    
-    start();
+
+    var game = new Game(canvas);
+    game.start();
 })()
